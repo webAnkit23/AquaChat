0f14da503b31bfcee4e000c9593c99aa2f79b1fa
webAnkit23 <152246497+webAnkit23@users.noreply.github.com>
2024-06-26 16:40:01 +0530

    Update socketContext.jsx

--- backend/frontend/src/context/socketContext.jsx
+++ backend/frontend/src/context/socketContext.jsx
@@ -11,7 +11,7 @@ export const SocketProvider = ({children}) =>{
    const {user} = useGlobal();
  useEffect(() =>{
        if(user==null)return;
-       socket.current = io('http://localhost:8000');
+       socket.current = io('/');
 
        socket.current.on('connect' ,() =>{
            console.log('connected');
